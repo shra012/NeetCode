fb5fd5b4959557c38e2bbfc817baf47d70d4f817
shra012 <shravan_fisher@yahoo.ca>
2026-01-16 18:35:00 -0800

    added same tree solution

---
+++ Blind-75/python/tree/same_tree.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p:
+            return False
+        if not q:
+            return False
+        if p.val != q.val:
+            return False
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+        return left and right
+
+if __name__ == "__main__":
+    p = TreeNode(1)
+    p.left = TreeNode(2)
+    p.right = TreeNode(3)
+
+    q = TreeNode(1)
+    q.left = TreeNode(2)
+    q.right = TreeNode(3)
+
+    solution = Solution()
+    print("Are the two trees the same?", solution.isSameTree(p, q))
